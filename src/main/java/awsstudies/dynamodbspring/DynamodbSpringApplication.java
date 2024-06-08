e5bc57a31d6823279a752f9d0246664c24f90734
thiago2santos <thisanti@hotmail.com>
2024-06-07 21:40:30 -0300

    First commit

---
+++ src/main/java/awsstudies/dynamodbspring/DynamodbSpringApplication.java
@@ -0,0 +1,13 @@
+package awsstudies.dynamodbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbSpringApplication.class, args);
+	}
+
+}
